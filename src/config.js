13a770fbbf03fe2823f6ecd1363b3ccc40d7e850
Andrea Pullen <apullenb@gmail.com>
2021-01-19 17:41:13 -0600

    error fix

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL
     || 'postgresql://dunder-mifflin@localhost/spaced-repetition',
-  JWT_SECRET: process.env.JWT_SECRET
+  JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 }
