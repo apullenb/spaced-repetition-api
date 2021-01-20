cf54b011689e4d7115e82e0038ffad6935c85145
Andrea Pullen <apullenb@gmail.com>
2021-01-19 18:11:21 -0600

    fix

--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -1,7 +1,7 @@
 const { NODE_ENV } = require('../config')
 
 module.exports = function errorHandler(error, req, res, next) {
-  const response = (NODE_ENV === 'production')
+  const response = 
     (console.error(error), { error: error.message, details: error })
 
   res.status(500).json(response)
